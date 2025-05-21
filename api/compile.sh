5407f838c6fc42810c3d592a9c3f60c3342f0fcd
juliaas2 <juliaas@al.insper.edu.br>
2025-05-21 17:36:08 -0300

    Adiciona Microserviços Originais

--- api/compile.sh
+++
@@ -1,6 +0,0 @@
-mvn -f account/pom.xml clean install -Dmaven.test.skip=true
-mvn -f auth/pom.xml clean install -Dmaven.test.skip=true
-
-mvn -f account-service/pom.xml clean package -Dmaven.test.skip=true
-mvn -f auth-service/pom.xml clean package -Dmaven.test.skip=true
-mvn -f gateway-service/pom.xml clean package -Dmaven.test.skip=true
